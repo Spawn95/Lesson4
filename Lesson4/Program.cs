f5a708b496c4a4d71dfbaadc95f9899bd08ede61
Spawn95 <disk-9@mail.ru>
2022-01-06 17:46:20 +0700

    Добавьте файлы проекта.

---
+++ Lesson4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first personal data");
+            string firstInfo = GetFullName();
+            
+            Console.WriteLine("Enter second personal data");
+            string secondInfo = GetFullName();
+            
+            Console.WriteLine("Enter third personal data");
+            string thirdInfo = GetFullName();
+            
+            Console.WriteLine($"Users:\n{firstInfo} \n{secondInfo} \n{thirdInfo}");
+        }
+        static string GetFullName()
+        {
+            Console.WriteLine("Please enter your firstName, lastName and patronymic");
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string patronymic = Console.ReadLine();
+            string infoUser = firstName + ' ' + lastName + ' ' + patronymic;
+            Console.WriteLine($"Hello {infoUser}");
+            return infoUser;
+        }
+    }
+}
